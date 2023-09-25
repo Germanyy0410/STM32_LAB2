e4e4e62b0756453e93a85bb2165da7c5ce1aeb77
Germanyy0410 <tienduchoang136@gmail.com>
2023-09-25 10:23:20 +0700

    done Ex3

--- STM32_PROJECT/Core/Src/main.c
+++ STM32_PROJECT/Core/Src/main.c
@@ -51,7 +51,8 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
-
+void display7SEG(int num);
+void update7SEG(int index);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -239,8 +240,11 @@ void display7SEG(int num){
 	if(num == 9) GPIOB->ODR = 0x04;
 }
 
+const int MAX_LED = 4;
+int index_led = 0;
+int led_buffer[4] = {1, 2, 3, 4};
+
 int counter = 50;
-int EN_State = 0;
 int evenCounter = 0;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
@@ -259,15 +263,22 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 
     HAL_GPIO_TogglePin(LED_RED_GPIO_Port, LED_RED_Pin);
 
-    switch (EN_State) {
+    if (index_led >= MAX_LED) {
+      index_led = 0;
+    }
+
+    update7SEG(index_led++);
+  }
+}
+
+void update7SEG(int index) {
+  switch (index) {
     case 0:
       HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, 0);
       HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
       HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
       HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-      display7SEG(1);
-
-      EN_State = 1;
+      display7SEG(led_buffer[0]);
       break;
 
     case 1:
@@ -275,9 +286,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
       HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 0);
       HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
       HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-      display7SEG(2);
-
-      EN_State = 2;
+      display7SEG(led_buffer[1]);
       break;
 
     case 2:
@@ -285,9 +294,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
       HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
       HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 0);
       HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 1);
-      display7SEG(3);
-
-      EN_State = 3;
+      display7SEG(led_buffer[2]);
       break;
 
     case 3:
@@ -295,13 +302,11 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
       HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, 1);
       HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, 1);
       HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, 0);
-      display7SEG(0);
-
-      EN_State = 0;
+      display7SEG(led_buffer[3]);
       break;
-    }
   }
 }
+
 /* USER CODE END 4 */
 
 /**
