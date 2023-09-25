7942d69d3cdda1f0f519cda72d200606ed601625
Germanyy0410 <tienduchoang136@gmail.com>
2023-09-25 12:59:21 +0700

    update Ex5

--- STM32_PROJECT/Core/Src/main.c
+++ STM32_PROJECT/Core/Src/main.c
@@ -226,19 +226,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void display7SEG(int num){
-	if(num == 0) GPIOB->ODR = 0x01;
-	if(num == 1) GPIOB->ODR = 0x4F;
-	if(num == 2) GPIOB->ODR = 0x12;
-	if(num == 3) GPIOB->ODR = 0x06;
-	if(num == 4) GPIOB->ODR = 0x4C;
-	if(num == 5) GPIOB->ODR = 0x24;
-	if(num == 6) GPIOB->ODR = 0x20;
-	if(num == 7) GPIOB->ODR = 0x0F;
-	if(num == 8) GPIOB->ODR = 0x00;
-	if(num == 9) GPIOB->ODR = 0x04;
-}
-
 int counter = 25;
 int evenCounter = 0;
 
@@ -288,6 +275,19 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 
     update7SEG(index_led++);
   }
+}
+
+void display7SEG(int num){
+	if(num == 0) GPIOB->ODR = 0x01;
+	if(num == 1) GPIOB->ODR = 0x4F;
+	if(num == 2) GPIOB->ODR = 0x12;
+	if(num == 3) GPIOB->ODR = 0x06;
+	if(num == 4) GPIOB->ODR = 0x4C;
+	if(num == 5) GPIOB->ODR = 0x24;
+	if(num == 6) GPIOB->ODR = 0x20;
+	if(num == 7) GPIOB->ODR = 0x0F;
+	if(num == 8) GPIOB->ODR = 0x00;
+	if(num == 9) GPIOB->ODR = 0x04;
 }
 
 void update7SEG(int index) {
